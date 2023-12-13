d61422a8e687e8df4c1c742526db39f348653cc5
elias baixas <elias.baixas@gmail.com>
2023-12-13 13:20:22 +0100

    Changed module name

--- generate.sh
+++ generate.sh
@@ -3,4 +3,4 @@ openapi-generator generate \
    -i https://raw.githubusercontent.com/openai/openai-openapi/master/openapi.yaml   \
    -g ruby   \
    --skip-validate-spec \
-   --additional-properties=gemName=openai-openapi-rb,gemVersion=1.0.0
+   --additional-properties=gemName=openai-openapi-rb,gemVersion=1.0.0,moduleName=OpenAI::API
